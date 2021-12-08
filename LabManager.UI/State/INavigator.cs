6084640e5c48c8b83dafc901ac967241f5a37b64
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-08 14:53:37 +0000

    View navigator added.

---
+++ LabManager.UI/State/INavigator.cs
@@ -0,0 +1,30 @@
+﻿using LabManager.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.State
+{
+    public interface INavigator
+    {
+        public ViewModelBase CurrentViewModel { get; set; }
+    }
+
+    public class Navigator : ObservableObject
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
--- LabManager.UI/ViewModels/NavigationViewModel.cs
+++ LabManager.UI/ViewModels/NavigationViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using LabManager.UI.Events;
+using Prism.Commands;
+using Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LabManager.UI.ViewModels
 {
-    public class NavigationViewModel
+    public class NavigationViewModel : ViewModelBase
     {
+        private IEventAggregator _eventAggregator;
+
+        public ICommand OpenViewCommand { get; private set; }
+
+        public NavigationViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            OpenViewCommand = new DelegateCommand<Type>(OnOpenViewExecute);
+        }
+
+        private void OnOpenViewExecute(Type viewModelType)
+        {
+            _eventAggregator.GetEvent<OpenViewEvent>()
+                .Publish(
+                new OpenViewEventArgs
+                {
+                    ViewModelName = viewModelType.Name
+                }
+                );
+        }
     }
 }
