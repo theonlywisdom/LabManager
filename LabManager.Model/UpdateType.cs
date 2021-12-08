241ae548bab6338c5bed6eb8b09b3e9b41ceda1c
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-08 10:50:54 +0000

    DbSeedContext and seed data added.

---
+++ LabManager.Model/UpdateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManager.Model
+{
+    public class UpdateType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public ICollection<Email> Emails { get; set; }
+    }
+}
