df0c8e03fb89a40a206acfeccd36b9de75ded219
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-08 14:53:37 +0000

    View navigator added.

--- LabManager.UI/ViewModels/ClientViewModel.cs
+++ LabManager.UI/ViewModels/ClientViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LabManager.UI.Events;
+using Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,18 @@ namespace LabManager.UI.ViewModels
 {
     public class ClientViewModel
     {
+        private IEventAggregator _eventAggregator;
+
+        public ClientViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenViewEvent>()
+                .Subscribe(OnOpenView);
+        }
+
+        private void OnOpenView(OpenViewEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
