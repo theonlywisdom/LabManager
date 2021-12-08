2e8d813f160a964f4cabd8124dffc9581579ce25
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-08 14:04:15 +0000

    IoC container added.

---
+++ LabManager.UI/StartUp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.StartUp
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+
+            return builder.Build();
+        }
+    }
+}
