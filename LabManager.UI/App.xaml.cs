8d702c4e521f78ed00a6ea50213fd557a90df6df
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-08 16:50:53 +0000

    App start up event handler implemented.

--- LabManager.UI/App.xaml.cs
+++ LabManager.UI/App.xaml.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LabManager.UI.StartUp;
 using System.Windows;
+using Autofac;
+
 
 namespace LabManager.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
