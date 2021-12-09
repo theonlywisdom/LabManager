cd3dda8b16feaeca4b74f4e5e3755cdccfb483d9
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-09 15:59:54 +0000

    Added client repository.

---
+++ LabManager.UI/Data/Repositories/ClientRepository.cs
@@ -0,0 +1,22 @@
+﻿using LabManager.DataAccess;
+using LabManager.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.Data.Repositories
+{
+    public class ClientRepository : GenericRepository<Client, DbSeedContext>, IClientRepository
+    {
+        public ClientRepository(DbSeedContext context) : base(context)
+        {
+        }
+
+        public override async Task<Client> GetByIdAsync(int id)
+        {
+            return await Context.Clients
+                .Include(c => c.Addresses)
+                .SingleAsync(c => c.Id == id);
+        }
+
+    }
+}
---
+++ LabManager.UI/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.Data.Repositories
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected readonly TContext Context;
+
+        public GenericRepository(TContext context)
+        {
+            this.Context = context;
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+    }
+}
