1d7fbeb081cf9d614a8ff8a2a286388af8d5c83b
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-13 14:32:26 +0000

    Authentication services added.

---
+++ LabManager.UI/Services/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using LabManager.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        //private readonly IAccountRepository
+        public Task<Account> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IAuthenticationService.RegistrationResult> Register(string email, string username, string password, string confirmPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LabManager.UI/Services/IAuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using LabManager.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManager.UI.Services
+{
+    public interface IAuthenticationService
+    {
+        public enum RegistrationResult
+        {
+            Success,
+            PasswordsDoNotMatch,
+            EmailAlreadyExists,
+            UsernameAlreadyExists
+        }
+
+        Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword);
+        Task<Account> Login(string username, string password);
+    }
+}
