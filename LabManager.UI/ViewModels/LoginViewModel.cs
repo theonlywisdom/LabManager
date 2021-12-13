6c1d3df21db7d11e74def3aef41c4097759c669d
Wisdom Baah <Wisdom.Baah@expressmicroscience.co.uk>
2021-12-13 14:09:42 +0000

    LoginViewModel added.

---
+++ LabManager.UI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using LabManager.UI.State;
+using System.Windows.Input;
+
+namespace LabManager.UI.ViewModels
+{
+    public class LoginViewModel : LoginViewModelBase
+    {
+        private string _username;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+    }
+
+    public class LoginViewModelBase : ViewModelBase
+    {
+
+    }
+
+}
